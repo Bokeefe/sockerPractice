b440a1726889534ab811b0ce589634623e240ab4
Brendan O'Keefe <brndnokf@yahoo.com>
2017-10-18 20:02:27 -0600

    pretty clean code now

--- server.js
+++ server.js
@@ -10,20 +10,31 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
 server.listen(80);
-let votes = ['brexit','Catalan']
+let votes = [];
+
+db.forEach((i)=> {
+    votes.push(i.voteName);
+});
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 
 app.post('/newVote', (req, res) => {
     var voteName = JSON.parse(req.body.voteName);
-    votes.push(voteName);
-    res.send(voteName);
+    console.log(votes.indexOf(voteName));
+    if(votes.indexOf(voteName) !== -1 ){
+        res.send("That name has already been used recently");
+    } else {
+        votes.push(voteName);
+        res.send(voteName);
+    }
 });
 
 app.post('/getVotes',(req,res)=>{
     res.send(votes);
 });
+
+
 app.post('/votePick', (req, res) => { 
     let voteName = req.body.votePick;
     res.send(voteName);
@@ -37,7 +48,7 @@ app.post('/votePick', (req, res) => {
     nsp.on('connection', function(socket){
       socket.join(voteName);
         if(!db[voteName]) {
-            db[voteName]={'yea':0,'nay':0,'cnctCount':1};
+            db[voteName]={'yea':0,'nay':0,'abs':0,'cnctCount':1};
             socket.emit('update',db[voteName]);
         } else {
             socket.emit('update',db[voteName]);
@@ -46,18 +57,24 @@ app.post('/votePick', (req, res) => {
         socket.on('yea', function () {
             db[voteName].yea++;
             socket.emit('update',db[voteName]);
-            save(db);
+            save(db,voteName);
           });
           socket.on('nay', function () {
             db[voteName].nay++;
             socket.emit('update',db[voteName]);   
-            save(db);
+            save(db,voteName);
+          });
+          socket.on('abs', function () {
+            db[voteName].abs++;
+            socket.emit('update',db[voteName]);   
+            save(db,voteName);
           });
     });
 });
 
-function save(x){
-    console.log(x);
-    x = JSON.stringify(x);   
-    fs.writeFileSync('./db.json', x);
+function save(entireDB,name){
+    console.log(entireDB[name]);
+
+    //x = JSON.stringify(x);   
+   // fs.writeFileSync('./db.json', x);
 }
